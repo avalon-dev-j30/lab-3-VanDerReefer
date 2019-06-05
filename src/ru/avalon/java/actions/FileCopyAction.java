635362d5f43aa50be42244c9f781ace11074123f
SESA540348 <SESA540348@WTRULVSE117168L.gmea.gad.schneider-electric.com>
2019-06-05 21:40:25 +0300

    TODO 1

--- src/ru/avalon/java/actions/FileCopyAction.java
+++ src/ru/avalon/java/actions/FileCopyAction.java
@@ -1,19 +1,40 @@
 package ru.avalon.java.actions;
 
+import java.nio.file.Path;
+
 /**
  * Действие, которое копирует файлы в пределах дискового
  * пространства.
  */
+
 public class FileCopyAction implements Action {
-    /**
-     * {@inheritDoc}
-     */
-    @Override
+    
+    private Path sourcePath;
+    private Path destinationPath;
+
+    public FileCopyAction(Path sourcePath, Path destinationPath) {
+        this.sourcePath = sourcePath;
+        this.destinationPath = destinationPath;
+    }
+    
+   @Override
     public void run() {
         /*
          * TODO №2 Реализуйте метод run класса FileCopyAction
          */
+        
+        
         throw new UnsupportedOperationException("Not implemented yet!");
+
+    @Override
+    public void start() {
+        Action.super.start(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void close() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     }
 
     /**
